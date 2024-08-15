1b2b0e15847ba54f93b46827ac9603211794be74
Modasser Hasan <manvstech2.0@gmail.com>
2024-08-15 17:36:46 +0600

    Sort by price and time addede

--- client-side/src/routes/Home.jsx
+++ client-side/src/routes/Home.jsx
@@ -10,6 +10,10 @@ const Home = () => {
   const [totalPages, setTotalPages] = useState(1);
   const itemsPerPage = 15;
 
+  // Filters
+  const [priceSortBy, setPriceSortBy] = useState("");
+  const [newestFirst, setNewestFirst] = useState(false);
+
   if (!productsDataMain.success) {
     return <div>ERROR!</div>;
   }
@@ -30,7 +34,7 @@ const Home = () => {
       const res = await fetch(
         `${
           import.meta.env.VITE_SERVER_URL
-        }/products?skip=${skip}&limit=${itemsPerPage}`
+        }/products?skip=${skip}&limit=${itemsPerPage}&priceSortBy=${priceSortBy}&newestFirst=${newestFirst}`
       );
 
       const data = await res.json();
@@ -38,10 +42,32 @@ const Home = () => {
       setPageLoading(false);
     };
     loadData();
-  }, [currentPage]);
+  }, [currentPage, priceSortBy, newestFirst]);
 
   return (
     <div>
+      <div className="mb-6 flex justify-end gap-5 items-center">
+        {/* Sort by Price */}
+        <label className="flex gap-5 items-center">
+          Sort By:
+          <select
+            value={priceSortBy}
+            onChange={(e) => setPriceSortBy(e.target.value)}
+            className="dark:bg-gray-800"
+          >
+            <option value="">Default</option>
+            <option value="high">High &gt; Low</option>
+            <option value="low">Low &gt; High</option>
+          </select>
+        </label>
+        {/* Sort by Newest */}
+        <button
+          className="btn btn-neutral"
+          onClick={() => setNewestFirst(!newestFirst)}
+        >
+          {newestFirst ? "Default" : "Newest First"}
+        </button>
+      </div>
       <div className="grid gap-4 lg:gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-between mb-10">
         {productsData.data.map((item) => (
           <ProductCard item={item} key={item._id} />
